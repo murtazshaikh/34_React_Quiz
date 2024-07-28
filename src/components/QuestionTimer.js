275832877ed354f9979015777d1a5af692d9f4cf
Murtaza Shaikh <95634721+murtazshaikh@users.noreply.github.com>
2024-07-28 23:40:50 +0530

    added react quiz project

---
+++ src/components/QuestionTimer.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+const QuestionTimer = ({ timeout, onTimeout, mode }) => {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [onTimeout, timeout]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <progress id="question-time" max={timeout} value={remainingTime} className={mode}/>;
+};
+
+export default QuestionTimer;
